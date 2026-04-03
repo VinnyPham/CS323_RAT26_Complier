f01a3dd1801c16dd5ae2a376a73a1304da766656
VinnyPham <vinnypham327@gmail.com>
2026-04-02 19:18:00 -0700

    Made parser

---
+++ Assignment1/parser.py
@@ -0,0 +1,17 @@
+# -------------------------------------------------------------
+# parser.py
+# 
+# Contains the Syntax Analyzer which checks if the token stream
+# from the lexical analyzer adheres to the language’s formal 
+# grammar rules.
+# -------------------------------------------------------------
+import lexer 
+
+#----parse-----------------------------------------------------
+# 
+#
+# Output:
+#       
+#--------------------------------------------------------------
+def parse():
+    pass 
